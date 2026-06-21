b1a0f066599e66b39028cde3a6bf0e0c638b1340
Nikhil <yourgmail@gmail.com>
2026-06-21 21:16:17 +0530

    Day-15 uploaded

---
+++ day-15/que60.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, count = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Move non-zero elements to the front
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            arr[count++] = arr[i];
+        }
+    }
+
+    // Fill remaining positions with zeroes
+    while (count < n) {
+        arr[count++] = 0;
+    }
+
+    printf("Array after moving zeroes to end:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
